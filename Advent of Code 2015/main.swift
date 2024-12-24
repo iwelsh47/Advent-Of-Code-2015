1b88710d7df53a73066ca0fe45ea43e18a06e83e
Ivan Welsh <8754457+iwelsh47@users.noreply.github.com>
2024-12-24 19:20:59 +1300

    Shuffle around some code so it makes extendable easier. Added Day 2 of 2015

--- Advent of Code 2015/main.swift
+++ Advent of Code 2015/main.swift
@@ -11,6 +11,24 @@ extension String {
     func trimmed() -> String { self.trimmingCharacters(in: .whitespacesAndNewlines) }
 }
 
+extension Array {
+    public func splat() -> (Element,Element) {
+        return (self[0],self[1])
+    }
+    
+    public func splat() -> (Element,Element,Element) {
+        return (self[0],self[1],self[2])
+    }
+    
+    public func splat() -> (Element,Element,Element,Element) {
+        return (self[0],self[1],self[2],self[3])
+    }
+    
+    public func splat() -> (Element,Element,Element,Element,Element) {
+        return (self[0],self[1],self[2],self[3],self[4])
+    }
+}
+
 func loadData(from filename: String) -> String {
     let filePath = "Advent of Code 2015/data/\(filename)"
     do {
@@ -22,62 +40,4 @@ func loadData(from filename: String) -> String {
     }
 }
 
-
-func Day01() {
-    /*
-     Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions
-     he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character
-     at a time.
-     
-     An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one
-     floor.
-     
-     The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
-     
-     For example:
-     
-     -  (()) and ()() both result in floor 0.
-     -  ((( and (()(()( both result in floor 3.
-     -  ))((((( also results in floor 3.
-     -  ()) and ))( both result in floor -1 (the first basement level).
-     -  ))) and )())()) both result in floor -3.
-     
-     To what floor do the instructions take Santa?
-     */
-    let input = loadData(from: "Day01")
-    
-    var currentFloor: Int = 0
-    input.forEach{
-        if $0 == "(" {
-            currentFloor += 1
-        } else {
-            currentFloor -= 1
-        }
-    }
-    print("Part one solution: \(currentFloor)")
-    
-    /*
-     Now, given the same instructions, find the position of the first character that causes him to enter the basement
-     (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
-     
-     For example:
-     
-     -  ) causes him to enter the basement at character position 1.
-     -  ()()) causes him to enter the basement at character position 5.
-     
-     What is the position of the character that causes Santa to first enter the basement?
-     */
-    currentFloor = 0
-    for c in input.enumerated() {
-        if c.element == "(" {
-            currentFloor += 1
-        } else {
-            currentFloor -= 1
-        }
-        if currentFloor == -1 {
-            print("Part two solution: \(c.offset + 1)")
-            break
-        }
-    }
-}
-
+runDay02()
